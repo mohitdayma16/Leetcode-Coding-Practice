ac8d9173b6f48a05d76a4689940dc1794d1da044
Mohitdayma <119912654+mohitdayma16@users.noreply.github.com>
2025-01-20 15:42:34 +0530

    Time: 23 ms (71.14%), Space: 66.4 MB (52.24%) - LeetHub

--- 2685-first-completely-painted-row-or-column/2685-first-completely-painted-row-or-column.java
+++ 2685-first-completely-painted-row-or-column/2685-first-completely-painted-row-or-column.java
@@ -1,46 +1,22 @@
 class Solution {
-
     public int firstCompleteIndex(int[] arr, int[][] mat) {
-        int numRows = mat.length, numCols = mat[0].length;
-        Map<Integer, Pair<Integer, Integer>> numToPos = new HashMap<>();
-       
-        for (int row = 0; row < numRows; row++) {
-            for (int col = 0; col < numCols; col++) {
-                int value = mat[row][col];
-                numToPos.put(value, new Pair(row, col));
+        int rows = mat.length, cols = mat[0].length;
+        Map<Integer, int[]> positionMap = new HashMap<>();
+        int[] rowCount = new int[rows];
+        int[] colCount = new int[cols];
+        Arrays.fill(rowCount, cols);
+        Arrays.fill(colCount, rows);
+        for (int r = 0; r < rows; ++r) {
+            for (int c = 0; c < cols; ++c) {
+                positionMap.put(mat[r][c], new int[]{r, c});
             }
         }
-
-        for (int i = 0; i < arr.length; i++) {
-            int num = arr[i];
-            Pair<Integer, Integer> pos = numToPos.get(num);
-            int row = pos.getKey();
-            int col = pos.getValue();
-            mat[row][col] = -mat[row][col]; 
-
-            if (checkRow(row, mat) || checkColumn(col, mat)) {
-                return i;
+        for (int idx = 0; idx < arr.length; ++idx) {
+            int[] pos = positionMap.get(arr[idx]);
+            if (--rowCount[pos[0]] == 0 || --colCount[pos[1]] == 0) {
+                return idx;
             }
         }
-
         return -1;
-    }
-
-    private boolean checkRow(int row, int[][] mat) {
-        for (int col = 0; col < mat[0].length; col++) {
-            if (mat[row][col] > 0) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    private boolean checkColumn(int col, int[][] mat) {
-        for (int row = 0; row < mat.length; row++) {
-            if (mat[row][col] > 0) {
-                return false;
-            }
-        }
-        return true;
     }
 }
