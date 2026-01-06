40864f1d2b0930fb7a417b06205f9ad118ea7435
Mohit Dayma <119912654+mohitdayma16@users.noreply.github.com>
2026-01-07 00:01:24 +0530

    Time: 3 ms (87.57%), Space: 43.8 MB (6.53%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,21 +1,28 @@
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
-        Map<Character, Character> mapping = new HashMap<>();
-        mapping.put(')', '(');
-        mapping.put('}', '{');
-        mapping.put(']', '[');
 
-        for (char c : s.toCharArray()) {
-            if (mapping.containsValue(c)) {
-                stack.push(c);
-            } else if (mapping.containsKey(c)) {
-                if (stack.isEmpty() || mapping.get(c) != stack.pop()) {
-                    return false;
+        for(char ch : s.toCharArray()){
+            if(ch == '(' || ch == '[' || ch == '{'){
+                stack.push(ch);
+            }else{
+                if(ch == ')'){
+                    if(stack.isEmpty() || stack.pop() != '('){
+                        return false;
+                    }
+                }
+                if(ch == ']'){
+                    if(stack.isEmpty() || stack.pop() != '['){
+                        return false;
+                    }
+                }
+                if(ch == '}'){
+                    if(stack.isEmpty() || stack.pop() != '{'){
+                        return false;
+                    }
                 }
             }
         }
-
-        return stack.isEmpty();        
+        return stack.isEmpty();
     }
 }
